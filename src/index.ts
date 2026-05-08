737ecc1ca6451163739de4d057abd4f4856c40ce
Mally <no-reply@mally.qzz.io>
2026-05-08 13:08:42 +0700

    create new CLI project

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { program } from 'commander';
+import { orderPizza } from './build';
+import * as fs from 'fs';
+import * as path from 'path';
+
+interface Options {
+  small: boolean;
+  pizzaType: string;
+}
+
+program
+  .version(JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), { encoding: 'utf-8' })).version)
+  .option('-s, --small', 'small pizza size')
+  .requiredOption('-p, --pizza-type <type>', 'flavour of pizza');
+
+program.parse(process.argv);
+
+const options = program.opts() as Options;
+
+orderPizza(options.small ?? false, options.pizzaType).then(console.log);
