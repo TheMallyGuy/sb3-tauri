616b57514a3639e965e6c74415a0422f6a527fbd
Mally <no-reply@mally.qzz.io>
2026-05-08 13:34:23 +0700

    refactor checks

--- src/check.ts
+++ src/check.ts
@@ -1,16 +1,16 @@
 import { exec } from "child_process";
 
-
-function testInstallation() {
-    try {
-        exec("rustup", (error, stdout, stderr) => {
-            if (error) {
-                console.log("You don't have Rustup installed. Please Follow Tauri Prerequisites and then try again.")
-            }
-        })
-
-    } catch (error) {
-        console.log(`Someting went wrong during checking your installation. Please make sure that you have met Tauri's prerequisites! \n Tauri Prerequisites : https://tauri.app/start/prerequisites \n Logs : ${error}`)
-        return
+export function testInstallation() { // more tests in the future
+  exec("rustup --version", (error, stdout, stderr) => {
+    if (error) {
+      console.log(
+        `You don't have Rustup installed. Please follow Tauri Prerequisites and then try again.\n` +
+        `Tauri Prerequisites: https://tauri.app/start/prerequisites\n` +
+        `Logs: ${stderr || error.message}`
+      );
+      return;
     }
+
+    console.log(`Health check complete with no errors. (${stdout.trim()})`);
+  });
 }
